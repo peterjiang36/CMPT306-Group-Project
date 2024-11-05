dc0490d1e17d4951cdbf4fe19305321953dd2dae
Charles L <charlesliu618@gmail.com>
2024-11-04 21:06:04 -0600

    Successfully add timer to the exam object

--- enemy.gd
+++ enemy.gd
@@ -3,11 +3,15 @@ class_name Enemy
 
 var timer = Timer.new()
 
+func _ready() -> void:
+	add_child(timer)
+	timer.timeout.connect(_on_Timer_timeout)
 
 func start_timer() -> void:
 	# Randomize the timer duration between 1 and 5 seconds
-	var duration = randf_range(1.0, 5.0)
+	var duration = randf_range(1.0, 10.0)
 	timer.start(duration)
+	print("timer is started")
 
 func _on_Timer_timeout() -> void:
 	
