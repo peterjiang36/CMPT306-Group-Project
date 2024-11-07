9772974b13fa3a68a85649cca3d46ec958abd189
Charles L <charlesliu618@gmail.com>
2024-11-07 14:41:37 -0600

    Adding Increment Features
    Auto increment
    Click Increment

---
+++ exam_builder.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+@onready var examButton = $"UI/Exam Paper"
+@onready var score = $"Score Label"
+var increment_score = 0
+
+var time_accumulator = 0.0  # Accumulate time to control increment speed
+var increment_interval = 1.0
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	score.text = str(increment_score)
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	time_accumulator += delta
+	
+	if time_accumulator >= increment_interval:
+		increment_score += 1  # Increment the score
+		score.text = str(increment_score)  # Update the score display
+		time_accumulator = 0.0  # Reset the accumulator
+
+
+func _on_exam_paper_pressed() -> void:
+	increment_score+=1
+	score.text = str(increment_score)
--- exam_paper.gd
+++ exam_paper.gd
@@ -9,7 +9,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
 
 
-func _on_button_down() -> void:
-	var Score = $"../Control/Score"
+
+func _on_pressed() -> void:
+	pass
